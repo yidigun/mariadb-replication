1a3c57ff10d0adb05221adfc2e4e350e64bff0b4
Daekyu Lee <dklee@yidigun.com>
2022-02-11 17:47:43 +0900

    make separated scripts

--- bin/backup-master.sh
+++ bin/backup-master.sh
@@ -2,5 +2,5 @@
 myname=`basename $0 .sh | sed -e 's!/!_!g'`
 
 snapshot=/snapshots/snapshot-`date +%Y%m%d`
-mariabackup --backup --target-dir=$snapshot && \
+mariabackup --backup --target-dir=$snapshot -u root && \
   mariabackup --prepare --target-dir=$snapshot
