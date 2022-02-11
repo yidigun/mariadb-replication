1c7f67cbc3fd25504f1867e3ce121e49ec02f949
Daekyu Lee <dklee@yidigun.com>
2022-02-11 20:25:13 +0900

    add status scripts

--- bin/show-status.sh
+++ bin/show-status.sh
@@ -1,14 +1,27 @@
 #!/bin/sh
+myname=`basename $0 .sh | sed -e 's!/!_!g'`
 
+echo
 echo "############### Server Version ###############"
-mysqladmin -uroot version
+echo
+mysqladmin -uroot version | sed -e "s/^/[mysqladmin] /"
 
+echo
 echo "############### Server Status ###############"
-mysqladmin -uroot status
+echo
+mysqladmin -uroot status | sed -e "s/^/[mysqladmin] /"
 
 if [ "$REPL_MODE" = master -o "$REPL_MODE" = slave ]; then
+  echo
   echo "############### Replication Status ###############"
+  echo
   echo "Replication Mode: $REPL_MODE"
+  echo
+
+  run_query() {
+    echo "$*" | sed -e "s/^/[mysql] /"
+    echo "$*" | mysql -uroot mysql
+  }
   case $REPL_MODE in
     master)
       run_query 'show master status\G'
@@ -20,5 +33,7 @@ if [ "$REPL_MODE" = master -o "$REPL_MODE" = slave ]; then
   esac
 fi
 
+echo
 echo "############### Connections ###############"
-netstat -natp | grep -v TIME_WAIT
+echo
+netstat -natp | grep -v TIME_WAIT | sed -e "s/^/[netstat] /"
