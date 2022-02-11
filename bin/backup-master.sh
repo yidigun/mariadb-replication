112a4b227c4004efacbc6f2d29b8974a385405ee
Daekyu Lee <dklee@yidigun.com>
2022-02-11 19:33:27 +0900

    fix slave scripts

--- bin/backup-master.sh
+++ bin/backup-master.sh
@@ -3,9 +3,9 @@ myname=`basename $0 .sh | sed -e 's!/!_!g'`
 
 snapshot=/snapshots/snapshot-`date +%Y%m%d`
 mariabackup --backup -u root --target-dir=$snapshot && \
-  mariabackup --prepare --target-dir=$snapshot && \
+  mariabackup --prepare --target-dir=$snapshot
 
 if [ -f $snapshot/xtrabackup_binlog_info ]; then
   chmod a+rx $snapshot && \
-  echo "[$myanem] backup createed: $snapshot"
+  echo "[$myname] backup createed: $snapshot"
 fi
