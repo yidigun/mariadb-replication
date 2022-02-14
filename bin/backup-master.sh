bd3986db0bee8c3f7c153f2ad44f702dd15a45b5
Daekyu Lee <dklee@yidigun.com>
2022-02-14 12:21:48 +0900

    add mariabackup arguments

--- bin/backup-master.sh
+++ bin/backup-master.sh
@@ -2,7 +2,7 @@
 myname=`basename $0 .sh | sed -e 's!/!_!g'`
 
 snapshot=/snapshots/snapshot-`date +%Y%m%d`
-(mariabackup --backup -u root --target-dir=$snapshot && \
+(mariabackup --backup -u root --target-dir=$snapshot "$@" && \
   mariabackup --prepare --target-dir=$snapshot) | \
     sed -e 's/^/[mariabackup] /'
 
